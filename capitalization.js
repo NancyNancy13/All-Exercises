323a5b079894023056031d665bf439481aa3ae42
Nancy <nanc0603@stud.kea.dk>
2022-08-29 23:08:47 +0200

    capitalization

---
+++ capitalization.js
@@ -0,0 +1,8 @@
+const givenName = "nancy";
+
+const startName = givenName[0].toUpperCase();
+console.log(startName);
+const endName = givenName.substring(1);
+console.log(endName);
+const newName = startName + endName;
+console.log(newName);
