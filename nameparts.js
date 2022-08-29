018b3cf8fdfead720a7f3b9158efd6aa5f426dc9
Nancy <nanc0603@stud.kea.dk>
2022-08-29 21:38:12 +0200

    name parts

---
+++ nameparts.js
@@ -0,0 +1,18 @@
+// using IndexOf
+
+const fullName = "Peter Heronimous Lind";
+const firstName = "Peter";
+const indexOfFirst = fullName.indexOf(firstName);
+console.log(firstName);
+
+const MiddleName = "Heronimous";
+const indexOfMiddle = fullName.indexOf(MiddleName);
+console.log(MiddleName);
+
+const LastName = "Lind";
+const indexOfLast = fullName.indexOf(LastName);
+console.log(LastName);
+// using Substrings
+console.log(fullName.substring(0, 6));
+console.log(fullName.substring(6, 16));
+console.log(fullName.substring(17, 22));
