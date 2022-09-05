2adb738ea6a90befa694121338a6225d559a7464
Nancy <nanc0603@stud.kea.dk>
2022-09-05 14:09:13 +0200

    capitalization function

--- capitalization.js
+++ capitalization.js
@@ -1,8 +1,14 @@
-const givenName = "nancy";
+// const givenName = "nancy";
 
-const startName = givenName[0].toUpperCase();
-console.log(startName);
-const endName = givenName.substring(1);
-console.log(endName);
-const newName = startName + endName;
-console.log(newName);
+// const startName = givenName[0].toUpperCase();
+// console.log(startName);
+// const endName = givenName.substring(1);
+// console.log(endName);
+// const newName = startName + endName;
+// console.log(newName);
+const str = "nAncy";
+capitalize(str);
+function capitalize(str) {
+  str = str[0].toUpperCase() + str.substring(1).toLowerCase();
+  console.log(str);
+}
