0da44e074a76378aa2d6d565b29a4c79af5708b2
Nancy <nanc0603@stud.kea.dk>
2022-09-05 15:23:34 +0200

    getting full name

--- nameparts.js
+++ nameparts.js
@@ -1,30 +1,43 @@
 // using IndexOf
 
-// const fullName = "Peter Heronimous Lind";
-// const firstName = fullName.substring(0, fullName.indexOf(" "));
+const fullName = "Peter Heronimous Lind";
+const firstName = fullName.substring(0, fullName.indexOf(" "));
 
 // console.log(firstName);
 
-// const MiddleName = fullName.substring(fullName.indexOf(" ") + 1, fullName.lastIndexOf(" "));
+const middleName = fullName.substring(fullName.indexOf(" ") + 1, fullName.lastIndexOf(" "));
 
 // console.log(MiddleName);
 
-// const LastName = fullName.substring(fullName.lastIndexOf(" ") + 1);
+const LastName = fullName.substring(fullName.lastIndexOf(" ") + 1);
 
 // console.log(LastName);
 
 // //////////////////////////////////////////////////////////////////////////////////
-const fullName = "Peter Lind";
-getNameParts(fullName);
-function getNameParts(fullName) {
-  const firstName = fullName.substring(0, fullName.indexOf(" "));
+// const fullName = "Peter Heronimous Lind";
+// getNameParts(fullName);
+// function getNameParts(fullName) {
+// const firstName = fullName.substring(0, fullName.indexOf(" "));
 
-  const middleName = fullName.substring(fullName.indexOf(" ") + 1, fullName.lastIndexOf(" "));
+// const middleName = fullName.substring(fullName.indexOf(" ") + 1, fullName.lastIndexOf(" "));
+///////////////////////////////////////////////
+//   if (middleName == null) {
+//     console.log(firstName, LastName);
+//   }else{
+// console.log(firstName, MiddleName, LastName);
+// }
+//////////////////////////////////
 
-  //   if (middleName === null) {
-  //     console.log(undefined);
-  //   }
-
-  const lastName = fullName.substring(fullName.lastIndexOf(" ") + 1);
-  console.log({ firstName, middleName, lastName });
+// const lastName = fullName.substring(fullName.lastIndexOf(" ") + 1);
+//   console.log({ firstName, middleName, lastName });
+// }
+// //////////////////// full name exercise//////////////////////////
+fullNamefun("Potter", "Harry", "James");
+function fullNamefun(LastName, firstName, middleName) {
+  const newFullName = firstName + " " + middleName + " " + LastName;
+  if (middleName == null) {
+    console.log(firstName, LastName);
+  } else {
+    console.log(newFullName);
+  }
 }
